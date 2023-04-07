4834f8c4e8d4ba7da3af8081309b418a7f3a5686
Aldocool275 <121561781+Aldocool275@users.noreply.github.com>
2023-04-07 13:39:53 +0530

    Project 3
    Python Program to print positive numbers in the list

---
+++ Project3.py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+print("List 1: ")
+for i in list1:
+    if i>0:
+        print(i,end=" ")
+
+print("\n")  
+print("List 2:")
+list2=[12,14,-95,3]
+for i in list2:
+    if i>0:
+        print(i,end=" ")
